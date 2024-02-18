cca82f2027dcde6f35ac2b82d18cb9c2f892f8c2
aziz-codes <azizisoffline@gmail.com>
2024-02-18 14:09:43 +0500

    no navbar on home page

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,18 +1,25 @@
+"use client";
 import Link from "next/link";
 import React from "react";
-
+import { usePathname } from "next/navigation";
 const Navbar = () => {
+  const pathName = usePathname();
+
   return (
-    <div className="h-14 flex w-full border-b items-center px-2 justify-between">
-      <h4>Navbar</h4>
-      <div className="flex items-center gap-8">
-        <Link href="/">Home</Link>
-        <Link href="/products">Products</Link>
-        <Link href="/about">About</Link>
-        <Link href="/users">Users</Link>
-        <Link href="/account">Account</Link>
-      </div>
-    </div>
+    <>
+      {pathName === "/" ? null : (
+        <div className="h-14 flex w-full border-b items-center px-2 justify-between">
+          <h4>Navbar</h4>
+          <div className="flex items-center gap-8">
+            <Link href="/">Home</Link>
+            <Link href="/products">Products</Link>
+            <Link href="/about">About</Link>
+            <Link href="/users">Users</Link>
+            <Link href="/account">Account</Link>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
